615b8fdc6f5d697f497176733bfcd2209219feb2
cearly08 <chris.early@kimley-horn.com>
2021-03-09 22:01:26 -0700

    Added Google Analytics to the site
    Added the Google Analytics script tag and event handler for the "page_path" parameter, which will log each route change in the site. The GA site is https://analytics.google.com and uses our kits.atms@gmail.com account.

--- SigOpsMetrics/SigOpsMetrics.SPA/ClientApp/src/app/app.component.ts
+++ SigOpsMetrics/SigOpsMetrics.SPA/ClientApp/src/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import{Router, NavigationEnd} from '@angular/router';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
+
 export class AppComponent  {
   title = 'app';
+  constructor(public router: Router){
+    this.router.events.subscribe(event => {
+      if(event instanceof NavigationEnd){
+        gtag('config', 'G-8WB20C5SBR',
+          {
+            'page_path': event.urlAfterRedirects
+          }
+        );
+      }
+    }
+  )}
 }
