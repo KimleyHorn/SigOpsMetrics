813e1c1647f1685975a3a6b7179a120c641c1323
Christian Untiedt <Christian.Untiedt@kimley-horn.com>
2021-03-29 17:26:38 -0500

    -updated throughput graphs

--- SigOpsMetrics/SigOpsMetrics.SPA/ClientApp/src/app/components/graphs/throughput-graph/throughput-graph.component.ts
+++ SigOpsMetrics/SigOpsMetrics.SPA/ClientApp/src/app/components/graphs/throughput-graph/throughput-graph.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FilterService } from 'src/app/services/filter.service';
 import { MetricsService } from 'src/app/services/metrics.service';
 import { SignalsService } from 'src/app/services/signals.service';
 
@@ -8,45 +9,99 @@ import { SignalsService } from 'src/app/services/signals.service';
   styleUrls: ['./throughput-graph.component.css']
 })
 export class ThroughputGraphComponent implements OnInit {
-  data:any[];
-
-  source: string = "main";
-  level: string = "cor";
-  interval: string = "mo";
-  measure: string = "tp";
+  source: string = 'main';
+  level: string = 'cor';
+  interval: string = 'mo';
+  measure: string = 'tp';
 
   dt = new Date();
-  start: string = (this.dt.getMonth() + 1) + "/" + (this.dt.getFullYear() - 1); 
+  start: string = (this.dt.getMonth() + 1) + '/' + (this.dt.getFullYear() - 1); 
   end: string = this.convertDate(this.dt);
 
-  public graph = {
-    data: [
-        { x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: {color: 'red'} },
-        { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' },
-    ],
-    layout: {width: 320, height: 240, title: 'A Fancy Plot'}
+  data: any[] = [];
+  corridors: any;
+
+  lineGraph: any = {
+    data: [],
+    layout: { 
+      title: 'Throughput (peak veh/hr)',
+      showlegend: false,
+      xaxis: {
+        title: 'Vehicles per Hour Trend'
+      }
+    }
   };
 
-  constructor(private metricsService: MetricsService, private signalsService: SignalsService) { }
+  barGraph: any = {
+    data: [],
+    layout: { 
+      showlegend: false,
+      xaxis: {
+        title: 'Vehicles per Hour Trend'
+      }
+    }
+  };
+
+  constructor(private metricsService: MetricsService, private signalsService: SignalsService, private filtersService: FilterService) { }
 
   ngOnInit(): void {
-    // this.signalsService.getData("corridors").subscribe(response => {
-
-    // });
-    
-    this.getData();
-    
-  }
-
-  getData(){
     this.metricsService.getMetrics(this.source, this.level, this.interval, this.measure, this.start, this.end).subscribe(response => {
       this.data = response;
+      this.corridors = new Set(this.data.filter(value => value['corridor'] !== null && value['zone_Group'] === 'All RTOP').map(data => data['corridor']));
       console.log(response);
+
+      this.loadBarGraph();
+      this.loadLineGraph();
     });
   }
 
-  unpack(rows, key){
-    return rows.map(function(row) { return row[key]; });
+  loadLineGraph(){
+    let graphData: any[] = [];
+    this.corridors.forEach(corridor => {
+      let filterData = this.data.filter(data => data['corridor'] === corridor);
+      let trace = {
+        name: corridor,
+        x: filterData.map(value => new Date(value['month'])),
+        y: filterData.map(value => value['vph']),
+        text: filterData.map(value => value['corridor']),
+        hovertemplate:
+          '<b>%{text}</b>' +
+          '<br>Week of: <b>%{x}</b>' +
+          '<br>Throughput (peak veh/hr): <b>%{y}</b>' +
+          '<extra></extra>',
+        mode: 'lines'
+      };
+
+      graphData.push(trace);
+    });
+
+    console.log(graphData);
+
+    this.lineGraph.data = graphData;
+  }
+
+  loadBarGraph(){
+    let graphData: any[] = [];
+    let currentMonth = new Date().getMonth();
+
+    this.corridors.forEach(corridor => {
+      let monthData = this.data.filter(data => data['corridor'] === corridor && new Date(data['month']).getMonth() === currentMonth);
+      let trace = {
+        name: corridor,
+        x: monthData.map(value => value['vph']),
+        y: monthData.map(value => value['corridor']),
+        orientation: 'h',
+        type: 'bar',
+        hovertemplate:
+          '<b>%{y}</b>' +
+          '<br>Throughput (peak veh/hr): <b>%{x}</b>' +
+          '<extra></extra>',
+      };
+
+      graphData.push(trace);
+    });
+
+    this.barGraph.data = graphData;
   }
 
   convertDate(date: Date): string{
--- SigOpsMetrics/SigOpsMetrics.SPA/ClientApp/src/app/services/filter.service.ts
+++ SigOpsMetrics/SigOpsMetrics.SPA/ClientApp/src/app/services/filter.service.ts
@@ -1,10 +1,13 @@
 import { Component, Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilterService {
+  private _filters = new BehaviorSubject<any>(null);
+  public filters = this._filters.asObservable();
 
   baseUrl: string;
   public signalGroups: Array<any> = [];
@@ -28,5 +31,9 @@ export class FilterService {
 
   public getSignalGroupsFromDb() {
     return this.http.get<any[]>(this.baseUrl + 'signals/zonegroups');
+  }
+
+  public setValue(key: string, value: any){
+    this._filters.next(null);
   }
 }
