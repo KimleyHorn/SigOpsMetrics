97d56b29bbd67efe706db6ad87644d35b4153738
Andrea Finch <andrea.finch@kimley-horn.com>
2021-06-01 12:40:51 -0400

    links updated to show icons

--- SigOpsMetrics/SigOpsMetrics.SPA/ClientApp/src/app/core/header/header.component.ts
+++ SigOpsMetrics/SigOpsMetrics.SPA/ClientApp/src/app/core/header/header.component.ts
@@ -11,14 +11,14 @@ export class HeaderComponent implements OnInit {
   
   constructor(private sideNav: SideNavService) { }
   links: any[] = [
-    {url:"https://traffic.dot.ga.gov/atspm", name:"ATSPM"},
-    {url:"http://www.511ga.org/", name:"Georgia 511"},
-    {url:"http://gdot-tmc-maxv/maxview/", name:"MaxView"},
-    {url:"https://navigator-atms.dot.ga.gov/", name:"Navigator"},
-    {url:"https://designitapps.com/GDOT/", name:"TEAMS"},
-    {url:"https://ritis.org/", name:"RITIS"},
-    {url:"https://gdotcitrix.dot.ga.gov/vpn/index.html", name:"GDOT Citrix"},
-    {url:"http://mygdot.dot.ga.gov/", name:"MyGDOT"},
+    {url:"https://traffic.dot.ga.gov/atspm", name:"ATSPM", icon: "..\\assets\\images\\icon_atspm.png"},
+    {url:"http://www.511ga.org/", name:"Georgia 511", icon: "..\\assets\\images\\icon_georgia511.png"},
+    {url:"http://gdot-tmc-maxv/maxview/", name:"MaxView", icon: "..\\assets\\images\\icon_maxview.png"},
+    {url:"https://navigator-atms.dot.ga.gov/", name:"Navigator", icon: "..\\assets\\images\\icon_navigator.jpg"},
+    {url:"https://designitapps.com/GDOT/", name:"TEAMS", icon: "..\\assets\\images\\icon_teams.png"},
+    {url:"https://ritis.org/", name:"RITIS", icon: "..\\assets\\images\\icon_ritis.jpg"},
+    {url:"https://gdotcitrix.dot.ga.gov/vpn/index.html", name:"GDOT Citrix", icon: "..\\assets\\images\\icon_citrix.png"},
+    {url:"http://mygdot.dot.ga.gov/", name:"MyGDOT", icon: "..\\assets\\images\\icon_gdot.png"},
 ]
 
   ngOnInit(): void {
