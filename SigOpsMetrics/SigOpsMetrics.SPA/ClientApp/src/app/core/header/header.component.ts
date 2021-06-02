be4c19ff8382cedee3f8a01c2217eb3d9faba096
Andrea Finch <andrea.finch@kimley-horn.com>
2021-06-02 17:36:54 -0400

    revised icon image and changed order in dialpad dropdown

--- SigOpsMetrics/SigOpsMetrics.SPA/ClientApp/src/app/core/header/header.component.ts
+++ SigOpsMetrics/SigOpsMetrics.SPA/ClientApp/src/app/core/header/header.component.ts
@@ -12,13 +12,12 @@ export class HeaderComponent implements OnInit {
   constructor(private sideNav: SideNavService) { }
   links: any[] = [
     {url:"https://traffic.dot.ga.gov/atspm", name:"ATSPM", icon: "..\\assets\\images\\icon_atspm.png"},
-    {url:"http://www.511ga.org/", name:"Georgia 511", icon: "..\\assets\\images\\icon_georgia511.png"},
+    {url:"https://gdotcitrix.dot.ga.gov/vpn/index.html", name:"GDOT Citrix", icon: "..\\assets\\images\\icon_citrix.png"},
+    {url:"http://www.511ga.org/", name:"Georgia 511", icon: "..\\assets\\images\\icon_gdot511.png"},
     {url:"http://gdot-tmc-maxv/maxview/", name:"MaxView", icon: "..\\assets\\images\\icon_maxview.png"},
     {url:"https://navigator-atms.dot.ga.gov/", name:"Navigator", icon: "..\\assets\\images\\icon_navigator.jpg"},
-    {url:"https://designitapps.com/GDOT/", name:"TEAMS", icon: "..\\assets\\images\\icon_teams.png"},
     {url:"https://ritis.org/", name:"RITIS", icon: "..\\assets\\images\\icon_ritis.jpg"},
-    {url:"https://gdotcitrix.dot.ga.gov/vpn/index.html", name:"GDOT Citrix", icon: "..\\assets\\images\\icon_citrix.png"},
-    {url:"http://mygdot.dot.ga.gov/", name:"MyGDOT", icon: "..\\assets\\images\\icon_gdot.png"},
+    {url:"https://designitapps.com/GDOT/", name:"TEAMS", icon: "..\\assets\\images\\icon_teams.png"}
 ]
 
   ngOnInit(): void {
