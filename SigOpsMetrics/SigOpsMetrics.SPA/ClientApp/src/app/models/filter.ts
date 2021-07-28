b8e6d377413a546bb09da5f0c654b0eac5823a09
Christian Untiedt <Christian.Untiedt@kimley-horn.com>
2021-07-28 12:32:51 -0500

    #120 date range and time period filters cannot be removed

--- SigOpsMetrics/SigOpsMetrics.SPA/ClientApp/src/app/models/filter.ts
+++ SigOpsMetrics/SigOpsMetrics.SPA/ClientApp/src/app/models/filter.ts
@@ -1,6 +1,7 @@
 export class Filter {
   //month: string;
   dateRange: number = 4;
+  timePeriod: number = 4;
   customStart: Date = null;
   customEnd: Date = null;
   daysOfWeek: string[] = null;
@@ -14,7 +15,6 @@ export class Filter {
   county: string = null;
   city: string = null;
   corridor: string = null;
-  timePeriod: number = 4;
 
   constructor() {
     // let _dt = new Date();
