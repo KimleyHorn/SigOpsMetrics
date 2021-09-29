61692170d64421267116702803d3bbbbb1b6e8ce
Knox Brand <knox.brand@kimley-horn.com>
2021-09-29 09:11:01 -0400

    Setup env varibles for dev/staging. Added prod site url for CORS.

--- SigOpsMetrics/SigOpsMetrics.SPA/ClientApp/src/environments/environment.prod.ts
+++ SigOpsMetrics/SigOpsMetrics.SPA/ClientApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_PATH: 'https://n0qgt05mk0.execute-api.us-east-1.amazonaws.com/dev/',
+  API_PATH: 'https://n0qgt05mk0.execute-api.us-east-1.amazonaws.com/prod/',
   mapCenterLat: 33.757776,
   mapCenterLon: -84.391578
 };
