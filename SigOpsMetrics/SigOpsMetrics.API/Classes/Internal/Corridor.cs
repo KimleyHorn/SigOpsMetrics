181f8db2966ea869bf0a05310339f242de8b7661
benpetru <107949255+benpetru@users.noreply.github.com>
2022-11-28 13:26:57 -0700

    Added comments

--- SigOpsMetrics/SigOpsMetrics.API/Classes/Internal/Corridor.cs
+++ SigOpsMetrics/SigOpsMetrics.API/Classes/Internal/Corridor.cs
@@ -8,6 +8,9 @@ namespace SigOpsMetrics.API.Classes
 {
     public class Corridor
     {
+        /// <summary>
+        /// The Corridor Id 
+        /// </summary>
         public string CorridorId { get; set; }
 
         /// <summary>
@@ -25,11 +28,30 @@ namespace SigOpsMetrics.API.Classes
         /// VPH, PD
         /// </example>
         public double CalculatedField { get; set; }
+
+        /// <summary>
+        /// This is currently not used but it was added to be consistent with the database and datatables used.
+        /// </summary>
         public double Ones { get; set; }
+
+        /// <summary>
+        /// The delta for the specific record.
+        /// </summary>
         public double Delta { get; set; }
+
+        /// <summary>
+        /// The Zone Group or Region.
+        /// </summary>
         public string ZoneGroup { get; set; }
+
+        /// <summary>
+        /// The description of the Corridor.
+        /// </summary>
         public string Description { get; set; }
 
+        /// <summary>
+        /// List of all the Signals associated with this Corridor.
+        /// </summary>
         public IEnumerable<Signal> Signals { get; set; } = Enumerable.Empty<Signal>();
     }
 }
