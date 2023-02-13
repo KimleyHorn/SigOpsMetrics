470185e70e4e115dcaa715299e78000845920b2b
Carter Liu <carter.liu@kimley-horn.com>
2023-02-13 13:20:58 -0700

    Update comments for datapull

--- SigOpsMetrics/SigOpsMetrics.API/Controllers/SignalsController.cs
+++ SigOpsMetrics/SigOpsMetrics.API/Controllers/SignalsController.cs
@@ -370,7 +370,7 @@ namespace SigOpsMetrics.API.Controllers
         }
 
         /// <summary>
-        /// Endpoint for performing daily data pull of corridors_latest.xls into sql table.
+        /// Endpoint for performing daily data pull of corridors_latest.xls into sql table. Destination 0 is AWS, 1 is GCP
         /// </summary>
         /// <returns></returns>
         [HttpGet("datapull/{key}/{destination}")]
