26c73f3cfce796778c0115415a64884511427c04
brandonhallKHA <135064507+brandonhallKHA@users.noreply.github.com>
2024-10-09 12:26:24 -0400

    ICalc commit for future implementation

---
+++ SigOpsMetrics/SigOpsMetricsCalcEngine/Calcs/ICalc.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Concurrent;
+using SigOpsMetricsCalcEngine.Models;
+
+namespace SigOpsMetricsCalcEngine.Calcs
+{
+    internal interface ICalc
+    {
+        public static Task<bool> Run(List<DateTime> validDates, ConcurrentBag<BaseEventLogModel> sigModels, string dir)
+        {
+            
+        }
+    }
+}
